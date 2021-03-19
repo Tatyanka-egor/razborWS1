0d68b15a8e7a14c25e0cdd849281c88826a8bfa6
student <Andrei>
2021-03-19 18:50:19 +0500

    xxx

--- app/src/main/java/com/example/razborws1/screens/LaunchScreen/launch_screen.java
+++ app/src/main/java/com/example/razborws1/screens/LaunchScreen/launch_screen.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.razborws1.R;
+import com.example.razborws1.screens.SignIn;
 import com.example.razborws1.screens.SignUp;
 
 public class launch_screen extends AppCompatActivity {
@@ -15,16 +16,20 @@ public class launch_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+
+
+
         new Handler().postAtTime(new Runnable(){
             @Override
             public void run(){
-                Intent intent=new Intent(launch_screen.this, SignUp.class);
+                Intent intent=new Intent(launch_screen.this, SignIn.class);
                 startActivity(intent);
                 finish();}
 
 
             }, 2000);
         }
+        //переход с задержкой заставка на рабочий экран порядок появления меняем в манифесте
 
 
 
--- app/src/main/java/com/example/razborws1/screens/SignUp.java
+++ app/src/main/java/com/example/razborws1/screens/SignUp.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.razborws1.R;
+import com.example.razborws1.databinding.ActivitySignUpBinding;
 
 public class SignUp extends AppCompatActivity {
 
+    ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
     }
 }
